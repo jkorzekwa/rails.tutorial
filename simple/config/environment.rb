a7f89adacbe523f8e6a681d07bf21e3d2e62762e
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-19 23:36:35 -0500

    migrate db on start

--- simple/config/environment.rb
+++ simple/config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Migrate db
+config.after_initialize do
+  ActiveRecord::Migrator.migrate(Rails.root.join("db/migrate"), nil)
+end
