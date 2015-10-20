3d9993c2de5f94bb61961865d8ca3cbbeecc1c56
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-20 00:20:52 -0500

    update install script

--- simple/install.sh
+++ simple/install.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /etc/simple/db
 sudo chmod -R 777 /etc/simple
 # Copy init script to /etc/init.d
 sudo chmod a+x simple_init
-sudo cp simplt_init /etc/init.d/simple
+sudo cp simple_init /etc/init.d/simple
 sudo update-rc.d simple defaults
 # Add simple app path to env vars
 cd ../
