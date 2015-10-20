0fa0255e0334a758055da1c44c36c8fa1abc86bf
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-20 00:14:02 -0500

    Add startup script

--- simple/install.sh
+++ simple/install.sh
@@ -2,4 +2,13 @@
 # Create db directory
 sudo mkdir -p /etc/simple/db
 sudo chmod -R 777 /etc/simple
+# Copy init script to /etc/init.d
+sudo chmod a+x simple_init
+sudo cp simplt_init /etc/init.d/simple
+sudo update-rc.d simple defaults
+# Add simple app path to env vars
+cd ../
+export SIMPLE_APP_HOME=`pwd`
+sudo echo "export SIMPLE_APP_HOME=`pwd`" >> /etc/*bashrc
+
 
