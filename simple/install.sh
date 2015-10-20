942a568dde8d76072fb90ce3fc2e231ad9411315
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-20 00:41:08 -0500

    update install script

--- simple/install.sh
+++ simple/install.sh
@@ -3,12 +3,12 @@
 sudo mkdir -p /etc/simple/db
 sudo chmod -R 777 /etc/simple
 # Copy init script to /etc/init.d
+sed -i "s/^export $SIMPLE_APP_HOME=.*/export $SIMPLE_APP_HOME=`pwd`/" simple_init
 sudo chmod a+x simple_init
 sudo cp simple_init /etc/init.d/simple
 sudo update-rc.d simple defaults
 # Add simple app path to env vars
 cd ../
 export SIMPLE_APP_HOME=`pwd`
-sudo echo "export SIMPLE_APP_HOME=`pwd`" >> /etc/*bashrc
 
 
