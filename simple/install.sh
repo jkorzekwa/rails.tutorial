fdb405e849b013ab833211e2b1c80d5264cb741b
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-20 01:51:31 -0500

    add simple app

--- simple/install.sh
+++ simple/install.sh
@@ -1,14 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/python
+import os
+import shutil
 # Create db directory
-sudo mkdir -p /etc/simple/db
-sudo chmod -R 777 /etc/simple
+try:
+    os.makedirs("/etc/simple/db",mode=0777)
+except OSError:
+    pass
 # Copy init script to /etc/init.d
-sed -i "s/^export $SIMPLE_APP_HOME=.*/export $SIMPLE_APP_HOME=`pwd`/" simple_init
-sudo chmod a+x simple_init
-sudo cp simple_init /etc/init.d/simple
-sudo update-rc.d simple defaults
-# Add simple app path to env vars
-cd ../
-export SIMPLE_APP_HOME=`pwd`
-
-
+os.chdir('..')
+pwd_str = os.getcwd()
+os.chdir('bin')
+with open('simple_init', 'r') as input_file, open('new_file', 'w') as output_file:
+    for line in input_file:
+        if 'export' in line.strip():
+            output_file.write('export SIMPLE_APP_HOME=%s\n' % (pwd_str))
+        else:
+            output_file.write(line)
+shutil.move('new_file','simple_init')
+shutil.copy('simple_init', '/etc/init.d/simple')
+os.system('update-rc.d simple defaults')
