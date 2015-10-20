71739ce556f894b0ec2a4977b4c6d5477b89c8c5
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-19 23:49:16 -0500

    another db migration attempt

--- simple/config/application.rb
+++ simple/config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Simple
   class Application < Rails::Application
+    config.after_initialize do
+      ActiveRecord::Migrator.migrate(Rails.root.join("db/migrate"), nil)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
