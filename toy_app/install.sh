29d760fa474c5745bc6f16f27e664e676ccfa512
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-21 00:56:07 -0500

    Fix install script

--- toy_app/install.sh
+++ toy_app/install.sh
@@ -7,8 +7,8 @@ if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
 
 # Create db directory
-if not os.path.exists('/etc/simple/db'):
-    os.makedirs("/etc/simple/db",mode=0777)
+if not os.path.exists('/etc/toy/db'):
+    os.makedirs("/etc/toy/db",mode=0777)
 
 # Copy init script to /etc/init.d
 os.chdir('..')
@@ -20,8 +20,8 @@ with open('app_init', 'r') as input_file, open('new_file', 'w') as output_file:
             output_file.write('export TOY_APP_HOME=%s\n' % (pwd_str))
         else:
             output_file.write(line)
-shutil.move('new_file','simple_init')
-os.chmod('simple_init',0777)
-shutil.copy('app_init', '/etc/init.d/simple')
-os.system('update-rc.d simple defaults')
+shutil.move('new_file','toy_init')
+os.chmod('toy_init',0777)
+shutil.copy('app_init', '/etc/init.d/toy')
+os.system('update-rc.d toy defaults')
 os.system('service toy start')
