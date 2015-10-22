73657947249f3067e8345f6d20421cdc35141e99
jkorzekwa <jkorzekwa@lifesize.com>
2015-10-22 00:45:22 -0500

    Add build.properties

--- toy_app/build.sh
+++ toy_app/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Run in root app directory
+APP=toy_app
+ARTIFACT_URL="http://knewrealitys.com:8081/artifactory"
 
 # Install needed packages
 sudo apt-get -y install nodejs rpm wget
@@ -13,25 +15,32 @@ jruby -S warble
 wget http://apache.mirrors.pair.com/tomcat/tomcat-8/v8.0.28/bin/apache-tomcat-8.0.28.tar.gz
 tar -xzf apache-tomcat-*.tar.gz
 rm -rf apache-tomcat-*.tar.gz
-mv apache-tomcat-* toy_app
+mv apache-tomcat-* $APP
 
 # Move war into tomcat
-mv toy_app.war toy_app/webapps/toy.war
-rm -rf toy_app/webapps/ROOT
-ln -s toy.war toy_app/webapps/ROOT.war
+mv "$APP".war $APP/webapps/
+rm -rf $APP/webapps/ROOT
+ln -s "$APP".war $APP/webapps/ROOT.war
 
 # Move required files into tomcat
-echo $VERSION > toy_app/version.txt
-mv install.sh toy_app/bin
-mv uninstall.sh toy_app/bin
-mv app_init toy_app/bin
-mv infrastructure toy_app/
-mkdir -p toy_app/conf/Catalina/localhost
-mv ROOT.xml toy_app/conf/Catalina/localhost
-chmod 777 toy_app/bin/*
-sed -i 's/port="8080"/port="8182"/g' toy_app/conf/server.xml
-sed '$iexport CATALINA_OPTS="-Xmx512m"' toy_app/bin/startup.sh
-sed '$iexport JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"' toy_app/bin/startup.sh
+mv install.sh $APP/bin
+mv uninstall.sh $APP/bin
+mv app_init $APP/bin
+mv infrastructure $APP/
+mkdir -p $APP/conf/Catalina/localhost
+mv ROOT.xml $APP/conf/Catalina/localhost
+chmod 777 $APP/bin/*
+sed -i 's/port="8080"/port="8182"/g' $APP/conf/server.xml
+sed '$iexport CATALINA_OPTS="-Xmx512m"' $APP/bin/startup.sh
+sed '$iexport JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"' $APP/bin/startup.sh
 
 # Package app
-tar -zcvf toy_app-$VERSION.tar.gz toy_app
+tar -zcvf $APP-$VERSION.tar.gz $APP
+
+# Create build.properties
+echo VERSION=$VERSION >> $APP/build.properties
+echo APP=$APP >> $APP/build.properties
+echo ARTIFACT=$APP-$VERSION.tar.gz >> $APP/build.properties
+echo ARTIFACT_REPO=$ARTIFACT_REPO >> $APP/build.properties
+echo ARTIFACT_URL=$ARTIFACT_URL >> $APP/build.properties
+
